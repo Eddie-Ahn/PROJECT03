357552297f202b57632a64ab15548b614766c709
Eddie-Ahn <sukin.ahn@gmail.com>
2022-04-18 00:37:49 +0900

    숙제 제출 완료버전

--- app.py
+++ app.py
@@ -21,15 +21,18 @@ def get_matjip():
     # 2 단계 : 그걸 클라이언트에 돌려준다.
     return jsonify({'result': 'success', 'matjip_list': matjip_list})
 
-@app.route('/api/save_bookmark', methods=["POST"])
-def save_bookmark():
-    bookmark_receive = request.form['bookmark_give']
-    title_receive = request.form['index_give']
-    doc = {
-        'bookmark_status' : bookmark_receive
-    }
-    db.matjips.update({'title' : title_receive}, {'$set': {doc}})
-    return jsonify({'result' : 'success', 'msg' : '북마크 저장완료'})
+@app.route('/like_matjip', methods=["POST"])
+def like_matjip():
+    # 즐겨찾기 like 를 저장하는 db 기능 만들기
+    title_receive = request.form['title_give']
+    address_receive = request.form['address_give']
+    action_receive = request.form['action_give'] #좋아하기 버튼을 눌렀는지, 좋아하기 해제 버튼을 눌렀는지에 대한 액션 구분
+    
+    if action_receive == 'like':
+        db.matjips.update_one({'title' : title_receive, 'address' : address_receive}, {'$set' : {'liked' : True}})  # 앞에는 {업데이트할 대상을 매칭하는 조건}, {'$set' : ~~} 는 해당 조건이면 이걸로 업데이트
+    else : 
+        db.matjips.update_one({'title' : title_receive, 'address' : address_receive}, {'$unset' : {'liked' : False}}) # 이건 action 상태가 like 가 아니면 liked 를 지워주기 , '$unset' 은 업데이트가 아닌, 필드 삭제하는 업데이트다. '$unset' 뒤에 삭제할는 필드 : '', 'false', 'true' 뭐를 넣던 간에 상관 없음. 
+    return jsonify({'result' : 'success'}) 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
